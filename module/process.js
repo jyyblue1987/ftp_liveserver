4a8ed549e4769519d8fc1874008c5bb4f823eafb
jyyblue <jyyblue1987@outlook.com>
2020-01-20 15:44:42 +0200

    [add] spy directory.

--- module/process.js
+++ module/process.js
@@ -1,8 +1,10 @@
+var fs = require('fs');
+const pathname = require('path');
 var config = require('./config');
 var mysql   = require('mysql');
 var xml2js   = require("xml2js");
 var parser = new xml2js.Parser();
-var fs = require('fs');
+var chokidar = require('chokidar');
 
 var xml = fs.readFileSync('config.xml', 'utf-8');
 
@@ -13,6 +15,7 @@ parser.parseString(xml, function(err, result) {
     config.MYSQL_CONFIG.user = result.settings.db_user[0];
     config.MYSQL_CONFIG.password = result.settings.db_password[0];
     config.MYSQL_CONFIG.database = result.settings.db_name[0];
+    config.FTP_DIR = result.settings.ftp_dir[0];
 
     handleDisconnect();
 });
@@ -26,7 +29,7 @@ function handleDisconnect() {
             setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
         }
         else {
-          
+            spyFileChanges();
         }
         // to avoid a hot loop, and to allow our node script to
     });                                     // process asynchronous requests in the meantime.
@@ -39,4 +42,33 @@ function handleDisconnect() {
             // throw err;                                  // server variable configures this)
         }
     });
+}
+
+function spyFileChanges()
+{
+    var ftp_dir = config.FTP_DIR;
+    var watcher = chokidar.watch(ftp_dir, {
+        ignored: /[\/\\]\./, 
+        persistent: true,
+        awaitWriteFinish: {
+            stabilityThreshold: 2000,
+            pollInterval: 100
+        },
+    });
+
+    watcher.on('add', function(path) {
+        console.log('File', path, 'has been added');
+        var filename = pathname.basename(path);
+    })
+    .on('addDir', function(path) {console.log('Directory', path, 'has been added');})
+    .on('change', function(path) {console.log('File', path, 'has been changed');})
+    .on('unlink', function(path) {console.log('File', path, 'has been removed');})
+    .on('unlinkDir', function(path) {console.log('Directory', path, 'has been removed');})
+    .on('error', function(error) {console.error('Error happened', error);})
+    
+    // 'add', 'addDir' and 'change' events also receive stat() results as second argument. 
+    // http://nodejs.org/api/fs.html#fs_class_fs_stats 
+    watcher.on('change', function(path, stats) {
+        console.log('File', path, 'changed size to', stats.size);       
+    });    
 }
